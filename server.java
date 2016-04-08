6326853dc3daf20b810ec9c64daa0dfb46141323
Oscar Zhen <ozhen@users.noreply.github.com>
2016-04-08 10:55:54 -0700

    SQLite is added

--- server.java
+++ server.java
@@ -1,13 +1,32 @@
 package cmpt213_hw6;
 
 import java.rmi.*;
+import java.sql.*;
 
 public class server {
 
 	public static void main(String args[]) throws Exception {
-		java.rmi.registry.LocateRegistry.createRegistry(1101);
+		
+		Connection c = null;
+			try {
+				Class.forName("org.sqlite.JDBC");
+				c = DriverManager.getConnection("jdbc:sqlite:test.db");
+				System.out.println("Opened database successfully");
+		      
+		      
+		      
+		      
+			} catch ( Exception e ) {
+		    		System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		    		System.exit(0);
+		    }
+		
+		  
+		
+		java.rmi.registry.LocateRegistry.createRegistry(1001);
 		incimpl inc = new incimpl();
 		//Naming.rebind("//localhost/inc", inc);
 		Naming.rebind("server", inc);
+		System.out.println("server is up!");
 	}
 }
