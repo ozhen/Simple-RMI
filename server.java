90bf0f584f08c87a019e0e2401ecb2c52656e88e
Oscar Zhen <ozhen@users.noreply.github.com>
2016-04-08 11:07:14 -0700

    SQLite Table is created

--- server.java
+++ server.java
@@ -8,12 +8,18 @@ public class server {
 	public static void main(String args[]) throws Exception {
 		
 		Connection c = null;
+		Statement stmt = null;
 			try {
 				Class.forName("org.sqlite.JDBC");
 				c = DriverManager.getConnection("jdbc:sqlite:test.db");
 				System.out.println("Opened database successfully");
-		      
-		      
+				
+				stmt = c.createStatement();
+				String sql = "CREATE TABLE DATA " +
+		                   "(ID INT PRIMARY KEY     NOT NULL," + " SALARY         REAL)"; 
+				stmt.executeUpdate(sql);
+				stmt.close();
+				c.close();
 		      
 		      
 			} catch ( Exception e ) {
@@ -21,9 +27,9 @@ public class server {
 		    		System.exit(0);
 		    }
 		
-		  
+		System.out.println("Table created successfully"); 
 		
-		java.rmi.registry.LocateRegistry.createRegistry(1001);
+		java.rmi.registry.LocateRegistry.createRegistry(1002);
 		incimpl inc = new incimpl();
 		//Naming.rebind("//localhost/inc", inc);
 		Naming.rebind("server", inc);
